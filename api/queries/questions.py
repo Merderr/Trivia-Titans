cdde34f3fbd7cee7b25127db1e8994a9619a4467
Taylor Pearce <taylor.a.pearce@gmail.com>
2023-11-15 12:52:54 -0700

    Get_ALL, Create Complete

--- api/queries/questions.py
+++ api/queries/questions.py
@@ -3,7 +3,24 @@ from pydantic import BaseModel
 from typing import Optional, List, Union
 
 
-class QuestionModel(BaseModel):
+class Error(BaseModel):
+    message: str
+
+
+class QuestionModelIn(BaseModel):
+    category: str
+    type: str
+    difficulty: str
+    question: str
+    correct_answer: str
+    incorrect_answers: List[str]
+
+    class Config:
+        orm_mode = True
+
+
+class QuestionModelOut(BaseModel):
+    id: int
     category: str
     type: str
     difficulty: str
@@ -16,29 +33,57 @@ class QuestionModel(BaseModel):
 
 
 class QuestionRepository:
-    def get_question(self) -> List[QuestionModel]:
+    def get_questions(self) -> Union[Error, List[QuestionModelOut]]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
-                        SELECT category, type, difficulty, question, correct_answer, incorrect_answers
+                        SELECT id, category, type, difficulty, question, correct_answer, incorrect_answers
                         FROM question
                         ORDER BY category;
                         """
                     )
-                    result = []
-                    for record in db:
-                        question = QuestionModel(
-                                category=record[0],
-                                type=record[1],
-                                difficulty=record[2],
-                                question=record[3],
-                                correct_answer=record[4],
-                                incorrect_answers=record[5],
+                    result_list = []
+                    for record in result:
+                        question = QuestionModelOut(
+                                id=record[0],
+                                category=record[1],
+                                type=record[2],
+                                difficulty=record[3],
+                                question=record[4],
+                                correct_answer=record[5],
+                                incorrect_answers=record[6] if isinstance(record[6], list) else [record[6]],
                         )
-                        result.append(question)
-                    return result
+                        result_list.append(question)
+                    return result_list
         except Exception as e:
             print(e)
-            return {"message": "Could not get question"}
+            return {"message": "Could not get questions"}
+
+    def create(self, question: QuestionModelIn) -> QuestionModelOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO question
+                        (category, type, difficulty, question, correct_answer, incorrect_answers)
+                    VALUES
+                        (%s, %s, %s, %s, %s, %s)
+                    RETURNING id;
+                    """,
+                    [
+                        question.category,
+                        question.type,
+                        question.difficulty,
+                        question.question,
+                        question.correct_answer,
+                        question.incorrect_answers,
+                    ],
+                )
+                id = result.fetchone()[0]
+                return self.question_in_to_out(id, question)
+
+    def question_in_to_out(self, id: int, question: QuestionModelIn):
+        old_data = question.dict()
+        return QuestionModelOut(id=id, **old_data)
--- api/routers/questions.py
+++ api/routers/questions.py
@@ -1,5 +1,5 @@
-from api.queries.questions import QuestionModel
-from api.queries.questions import QuestionRepository
+from queries.questions import QuestionModelIn, QuestionModelOut, Error
+from queries.questions import QuestionRepository
 from fastapi import APIRouter, Depends, Response
 from typing import Union, List, Optional
 
@@ -9,8 +9,28 @@ router = APIRouter()
 TRIVIA_API = "https://opentdb.com/api.php?amount=1&type=multiple"
 
 
-@router.get("/question", response_model=List[QuestionModel])
-def get_question(
-    repo:  QuestionRepository = Depends(TRIVIA_API),
+@router.get("/questions", response_model=Union[List[QuestionModelOut], Error])
+def get_questions(
+    repo:  QuestionRepository = Depends(),
 ):
-    return repo.get_question()
+    return repo.get_questions()
+
+
+@router.post("/questions", response_model=Union[QuestionModelOut, Error])
+def create_question(
+    question: QuestionModelIn,
+    response: Response,
+    repo: QuestionRepository = Depends()
+):
+    return repo.create(question)
+
+@router.get("/questions/{question_id}", response_model=Optional[QuestionModelOut])
+def get_one_question(
+    question_id: int,
+    response: Response,
+    repo: QuestionRepository = Depends(),
+) -> Optional[QuestionModelOut]:
+    question = repo.get_one(question_id)
+    if question is None:
+        response.status_code = 404
+    return question
