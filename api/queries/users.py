eb8a1aae451644ff5533373a7663ab6373407170
Seth Porche <porchest2022@outlook.com>
2023-11-16 14:26:37 -0500

    added user table to migrations

--- api/queries/users.py
+++ api/queries/users.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-from passlib.hash import bcrypt
 import psycopg
 
 DATABASE_URL = (
@@ -15,44 +14,19 @@ class UserModel(BaseModel):
     name: str
     score: int
 
-    def verify_password(self, plain_password: str):
-        return bcrypt.verify(plain_password, self.password)
 
-    @classmethod
-    def create_password_hash(cls, password: str):
-        return bcrypt.hash(password)
-
-
-def create_users_table():
-    query = """
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            username TEXT UNIQUE,
-            password TEXT,
-            name TEXT,
-            score INTEGER
-        )
-    """
-    cursor.execute(query)
-    conn.commit()
+# def get_user_by_username(username: str):
+#     query = "SELECT * FROM users WHERE username = %s"
+#     with conn.cursor() as cursor:
+#         cursor.execute(query, (username,))
+#         return cursor.fetchone()
 
 
-def shutdown_db():
-    conn.close()
-
-
-def get_user_by_username(username: str):
-    query = "SELECT * FROM users WHERE username = %s"
-    with conn.cursor() as cursor:
-        cursor.execute(query, (username,))
-        return cursor.fetchone()
-
-
-def get_all_users():
-    query = "SELECT * FROM users"
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-        return cursor.fetchall()
+# def get_all_users():
+#     query = "SELECT * FROM users"
+#     with conn.cursor() as cursor:
+#         cursor.execute(query)
+#         return cursor.fetchall()
 
 
 def create_user(username: str, password: str, name: str, score: int):
@@ -67,22 +41,22 @@ def create_user(username: str, password: str, name: str, score: int):
         return cursor.fetchone()
 
 
-def update_user(username: str, new_name: str, new_score: int):
-    query = """
-        UPDATE users
-        SET name = %s, score = %s
-        WHERE username = %s
-        RETURNING *
-    """
-    with conn.cursor() as cursor:
-        cursor.execute(query, (new_name, new_score, username))
-        conn.commit()
-        return cursor.fetchone()
+# def update_user(username: str, new_name: str, new_score: int):
+#     query = """
+#         UPDATE users
+#         SET name = %s, score = %s
+#         WHERE username = %s
+#         RETURNING *
+#     """
+#     with conn.cursor() as cursor:
+#         cursor.execute(query, (new_name, new_score, username))
+#         conn.commit()
+#         return cursor.fetchone()
 
 
-def delete_user(username: str):
-    query = "DELETE FROM users WHERE username = %s RETURNING *"
-    with conn.cursor() as cursor:
-        cursor.execute(query, (username,))
-        conn.commit()
-        return cursor.fetchone()
+# def delete_user(username: str):
+#     query = "DELETE FROM users WHERE username = %s RETURNING *"
+#     with conn.cursor() as cursor:
+#         cursor.execute(query, (username,))
+#         conn.commit()
+#         return cursor.fetchone()
