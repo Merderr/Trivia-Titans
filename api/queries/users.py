fa6246b8cc20a17504109df5388bea32ce38d104
Seth Porche <porchest2022@outlook.com>
2023-11-20 16:06:30 -0500

    added seed data to sql file

--- api/queries/users.py
+++ api/queries/users.py
@@ -32,18 +32,29 @@ class UserModelOut(BaseModel):
 
 
 class UserRepository:
-    # def get_user_by_username(username: str):
-    #     query = "SELECT * FROM users WHERE username = %s"
-    #     with conn.cursor() as cursor:
-    #         cursor.execute(query, (username,))
-    #         return cursor.fetchone()
-
-    # def get_all_users(self):
-    #     query = "SELECT * FROM users"
-    #     with pool.connection as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(query)
-    #             return cur.fetchall()
+    def get_one_user(self, user_id: int) -> Optional[UserModelOut]:
+        try:
+            # connect to the database
+            with pool.connection() as conn:
+                # get a named cursor (something to run SQL with)
+                with conn.cursor(name="get_one_user") as db:
+                    # Run our SELECT statement
+                    db.execute(
+                        """
+                        SELECT id, username, password, name, score
+                        FROM users
+                        WHERE id = %s
+                        """,
+                        [user_id],
+                    )
+                    record = db.fetchone()
+                    if record is None:
+                        return None
+                    print(record)
+                    return self.record_to_user_out(record)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not find that user"}
 
     def get_all_users(self) -> Union[Error, List[UserModelOut]]:
         try:
@@ -114,6 +125,15 @@ class UserRepository:
     def user_in_to_out(self, id: int, user: UserModelIn):
         old_data = user.dict()
         return UserModelOut(id=id, **old_data)
+
+    def record_to_user_out(self, record):
+        return UserModelOut(
+            id=record[0],
+            username=record[1],
+            password=record[2],
+            name=record[3],
+            score=record[4],
+        )
 
     # Mason Added this dont want to mess u up seth move down if needed
     # def get_leaderboard():
