9b462d3b58fe0da42f8f7c16efd64c7ab2d4a6b5
Seth Porche <porchest2022@outlook.com>
2023-11-29 16:18:23 -0500

    Fixed My Account to accurately reflect current user score

--- ghi/src/components/MyAccount.jsx
+++ ghi/src/components/MyAccount.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './MyAccount.css'; 
+import React, { useState, useEffect } from "react";
+import "./MyAccount.css";
 
 const hostURL = import.meta.env.VITE_REACT_APP_API_HOST;
 
 const MyAccount = () => {
   const [userData, setUserData] = useState(null);
-  const [storageUser, setStorageUser] = useState();
   const [loading, setLoading] = useState(true);
+  const [storageUser, setStorageUser] = useState(null);
 
   const fetchUserData = async () => {
     try {
@@ -17,14 +17,15 @@ const MyAccount = () => {
 
       if (response.ok) {
         const data = await response.json();
+        console.log("User Data:", data); // Log the data to the console
         setUserData(data);
       } else {
-        console.error('Failed to fetch user data');
-        alert('Oops! Something went wrong. Please try again later.');
+        console.error("Failed to fetch user data");
+        alert("Oops! Something went wrong. Please try again later.");
       }
     } catch (error) {
-      console.error('Error fetching user data:', error);
-      alert('Oops! Something went wrong. Please try again later.');
+      console.error("Error fetching user data:", error);
+      alert("Oops! Something went wrong. Please try again later.");
     } finally {
       setLoading(false);
     }
@@ -32,23 +33,27 @@ const MyAccount = () => {
 
   useEffect(() => {
     fetchUserData();
-    setStorageUser(JSON.parse(localStorage.getItem("user")));
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    console.log("Stored User:", storedUser);
+    setStorageUser(storedUser);
   }, []);
 
   return (
     <div className="account-container">
       {loading && <p>Loading...</p>}
-      {userData && (
-        <div className="account-card" >
+      {storageUser && (
+        <div className="account-card">
           <p className="account-title">Hello, {storageUser.name}</p>
           <div className="account-info">
             <span className="info-label">Username:</span>
             <span className="info-value">{storageUser.username}</span>
           </div>
-          <div className="account-info">
-            <span className="info-label">High Score:</span>
-            <span className="info-value">{storageUser.score}</span>
-          </div>
+          {userData && (
+            <div className="account-info">
+              <span className="info-label">High Score:</span>
+              <span className="info-value">{storageUser.score}</span>
+            </div>
+          )}
         </div>
       )}
     </div>
