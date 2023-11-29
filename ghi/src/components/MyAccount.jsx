b0417955f4dba6dbecfe722cb3897f9c49ee5608
mason <mason.doney@outlook.com>
2023-11-29 11:25:44 -0600

    My account styles

--- ghi/src/components/MyAccount.jsx
+++ ghi/src/components/MyAccount.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import './MyAccount.css'; 
 
 const hostURL = import.meta.env.VITE_REACT_APP_API_HOST;
 
 const MyAccount = () => {
   const [userData, setUserData] = useState(null);
   const [storageUser, setStorageUser] = useState();
+  const [loading, setLoading] = useState(true);
+
   const fetchUserData = async () => {
     try {
       const response = await fetch(`${hostURL}/api/users`, {
@@ -17,24 +20,35 @@ const MyAccount = () => {
         setUserData(data);
       } else {
         console.error('Failed to fetch user data');
+        alert('Oops! Something went wrong. Please try again later.');
       }
     } catch (error) {
       console.error('Error fetching user data:', error);
+      alert('Oops! Something went wrong. Please try again later.');
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchUserData();
-    setStorageUser(JSON.parse(localStorage.getItem("user")))
+    setStorageUser(JSON.parse(localStorage.getItem("user")));
   }, []);
 
   return (
-    <div>
+    <div className="account-container">
+      {loading && <p>Loading...</p>}
       {userData && (
-        <div>
-          <p>Hello, {storageUser.name}</p>
-          <p>Username: {storageUser.username}</p>
-          <p>High Score: {storageUser.score}</p>
+        <div className="account-card" >
+          <p className="account-title">Hello, {storageUser.name}</p>
+          <div className="account-info">
+            <span className="info-label">Username:</span>
+            <span className="info-value">{storageUser.username}</span>
+          </div>
+          <div className="account-info">
+            <span className="info-label">High Score:</span>
+            <span className="info-value">{storageUser.score}</span>
+          </div>
         </div>
       )}
     </div>
