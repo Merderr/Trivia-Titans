cf4838f1fe785bf05548215710c475b6ed6e7606
Hamza <hamzasahmed11@gitlab.com>
2023-11-29 16:10:58 -0500

    fixed about us

--- ghi/src/components/MyAccount.jsx
+++ ghi/src/components/MyAccount.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './MyAccount.css'; 
+import React, { useState, useEffect } from "react";
+import "./MyAccount.css";
 
 const hostURL = import.meta.env.VITE_REACT_APP_API_HOST;
 
@@ -19,12 +19,12 @@ const MyAccount = () => {
         const data = await response.json();
         setUserData(data);
       } else {
-        console.error('Failed to fetch user data');
-        alert('Oops! Something went wrong. Please try again later.');
+        console.error("Failed to fetch user data");
+        alert("Oops! Something went wrong. Please try again later.");
       }
     } catch (error) {
-      console.error('Error fetching user data:', error);
-      alert('Oops! Something went wrong. Please try again later.');
+      console.error("Error fetching user data:", error);
+      alert("Oops! Something went wrong. Please try again later.");
     } finally {
       setLoading(false);
     }
@@ -39,15 +39,21 @@ const MyAccount = () => {
     <div className="account-container">
       {loading && <p>Loading...</p>}
       {userData && (
-        <div className="account-card" >
-          <p className="account-title">Hello, {storageUser.name}</p>
+        <div className="account-card">
+          <p className="account-title">
+            Hello, {storageUser && storageUser.name}
+          </p>
           <div className="account-info">
             <span className="info-label">Username:</span>
-            <span className="info-value">{storageUser.username}</span>
+            <span className="info-value">
+              {storageUser && storageUser.username}
+            </span>
           </div>
           <div className="account-info">
             <span className="info-label">High Score:</span>
-            <span className="info-value">{storageUser.score}</span>
+            <span className="info-value">
+              {storageUser && storageUser.score}
+            </span>
           </div>
         </div>
       )}
