2ec5f7ec7e951cbc6578190b04ca8ac65ce19ee7
Seth Porche <porchest2022@outlook.com>
2023-11-29 12:49:41 -0500

    stylized leaderboard

--- ghi/src/components/Leaderboard.jsx
+++ ghi/src/components/Leaderboard.jsx
@@ -6,26 +6,26 @@ const hostURL = import.meta.env.VITE_REACT_APP_API_HOST;
 const Leaderboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
 
-  const get_leaderboard = async () => {
+  const getLeaderboard = async () => {
     const url = hostURL + "/leaderboard/";
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
       const sortedData = data.sort((a, b) => b.score - a.score);
-      setLeaderboard(data);
+      setLeaderboard(sortedData);
     }
   };
 
   useEffect(() => {
-    get_leaderboard();
+    getLeaderboard();
   }, []);
 
   return (
     <div className="leaderboard-container">
       <h2>Leaderboard</h2>
-      <ol>
+      <ol className="leaderboard-list">
         {leaderboard.map((user) => (
-          <li key={user.name}>
+          <li key={user.name} className="leaderboard-item">
             {user.name} - Score: {user.score}
           </li>
         ))}
