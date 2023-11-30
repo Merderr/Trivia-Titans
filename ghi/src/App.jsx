7f07a5c313094ff1bd9ede9fe7be6c774a50855a
Hamza <hamzasahmed11@gitlab.com>
2023-11-30 14:08:57 -0500

    fixed local changes

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import NavBar from "./NavBar";
 import PlayButton from "./components/PlayButton";
 import Signup from "./components/SignUp";
@@ -16,31 +15,30 @@ import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const domain = /https:\/\/[^/]+/;
+  const domain = /https?:\/\/[^/]+/;
   const basename = import.meta.env.VITE_PUBLIC_URL.replace(domain, "");
   return (
-    <BrowserRouter basename={basename}>
+    <Router basename={basename}>
       <AuthProvider baseUrl={import.meta.env.VITE_REACT_APP_API_HOST}>
-        <Router>
-          <>
-            <header className="app-header">
-              <NavBar />
-            </header>
-            <div className="content-container">
-              <Routes>
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/leaderboard" element={<Leaderboard />} />
-                <Route path="/myaccount" element={<MyAccount />} />
-                <Route path="/aboutus" element={<AboutUs />} />
-                <Route path="/play" element={<Game />} />
-                <Route path="/logout" element={<Logout />} />
-              </Routes>
-            </div>
-          </>
-        </Router>
+        <>
+          <header className="app-header">
+            <NavBar />
+          </header>
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="/myaccount" element={<MyAccount />} />
+              <Route path="/aboutus" element={<AboutUs />} />
+              <Route path="/play" element={<Game />} />
+              <Route path="/logout" element={<Logout />} />
+            </Routes>
+          </div>
+        </>
       </AuthProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
 
