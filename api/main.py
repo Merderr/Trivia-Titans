ce31d853120a5f2d1f188443ec90672251e0998e
Hamza <hamzasahmed11@gitlab.com>
2023-11-30 14:37:41 -0500

    fixing cors

--- api/main.py
+++ api/main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=[
+        os.environ.get(
+            "https://rc678-galvanize.gitlab.io", "http://localhost:3000"
+        )
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
