a480d52d85685cb6cc07540e014c14a910865682
mason <mason.doney@outlook.com>
2023-12-07 09:09:26 -0600

    testing tests

---
+++ api/tests/test_config.py
@@ -0,0 +1,6 @@
+
+import os
+
+class TestConfig:
+    DATABASE_URL = "postgresql://exampleuser:secret@postgres/postgrestriviadata"
+    SIGNING_KEY = "AC8C8396F9F516FCE01A6805FF8C3D8E"
--- api/tests/test_get_users.py
+++ api/tests/test_get_users.py
@@ -1,6 +1,11 @@
+# test_get_users.py
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi.testclient import TestClient
 from main import app
 from queries.users import UserRepository
+from test_config import TestConfig  # Import the testing configuration
 
 client = TestClient(app)
 
@@ -12,10 +17,11 @@ def test_get_all_users():
         "score": 100,
     }
 
-    with UserRepository() as repo:
+    # Use the testing configuration for the UserRepository
+    with UserRepository(config=TestConfig) as repo:
         hashed_password = "hashed_test_password"
         repo.create_user(test_user, hashed_password)
-    
+
     response = client.get("/api/users")
     assert response.status_code == 200
     assert test_user in response.json()
