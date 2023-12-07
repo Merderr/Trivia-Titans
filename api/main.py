c6ac66cca6fc7d34ed3de1a531052c48cb08ee30
Hamza <hamzasahmed11@gitlab.com>
2023-12-07 11:15:01 -0500

    fixing ci/cd

--- api/main.py
+++ api/main.py
@@ -11,6 +11,7 @@ origins = [
     "http://localhost:3000",
     "https://aug-2023-2-pt-api.mod3projects.com",
     "https://ice-climbers.gitlab.io/",
+    "https://module3-project-gamma-ice-climbers-c0a68aa6a822a297eec1a4775dd1.gitlab.io/",
     os.environ.get("CORS_HOST", None),
 ]
 
