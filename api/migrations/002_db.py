945bbf9caabcc2edbf8160e6cb10699e4f6501f0
Seth Porche <porchest2022@outlook.com>
2023-12-07 17:14:15 -0500

    db migrations edited

--- api/migrations/002_db.py
+++ api/migrations/002_db.py
@@ -2,13 +2,18 @@ steps = [
     [
         # "Up" SQL statement
         """
+        DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
         CREATE TABLE users (
-        id SERIAL PRIMARY KEY NOT NULL,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        name TEXT NOT NULL,
-        score INTEGER
+            id SERIAL PRIMARY KEY NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            name TEXT NOT NULL,
+            score INTEGER
         );
+    END IF;
+END $$;
         """,
         # "Down" SQL statement
         """
@@ -18,6 +23,9 @@ steps = [
     [
         # "Up" SQL statement
         """
+        DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'questions') THEN
         CREATE TABLE questions (
           id SERIAL PRIMARY KEY,
           category VARCHAR(1000) NOT NULL,
@@ -29,10 +37,12 @@ steps = [
           incorrect_answer_2 VARCHAR(1000) NOT NULL,
           incorrect_answer_3 VARCHAR(1000) NOT NULL
         );
+            END IF;
+END $$;
         """,
         # "Down" SQL statement
         """
-        DROP TABLE IF EXISTS question;
+        DROP TABLE IF EXISTS questions;
         """,
     ],
 ]
