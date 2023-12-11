bb265adfcdd6de86571379b4ac872df3256dd7f5
Hamza <hamzasahmed11@gitlab.com>
2023-12-11 09:37:34 -0500

    update ci/cd

--- api/tests/test_get_all_users.py
+++ api/tests/test_get_all_users.py
@@ -3,6 +3,14 @@ from main import app
 from queries.users import UserRepository
 
 
+DATABASE_URL = (
+    "postgresql://exampleuser:secret@"
+    "aug-2023-2-pt-db-service.default.svc.cluster.local/"
+    "postgrestriviadata"
+)
+SIGNING_KEY = "AC8C8396F9F516FCE01A6805FF8C3D8E"
+
+
 # Seth's Unit Test
 
 
--- api/tests/test_question.py
+++ api/tests/test_question.py
@@ -4,6 +4,13 @@ from main import app
 from queries.questions import QuestionRepository, QuestionModelOut
 
 
+DATABASE_URL = (
+    "postgresql://exampleuser:secret@"
+    "aug-2023-2-pt-db-service.default.svc.cluster.local/"
+    "postgrestriviadata"
+)
+SIGNING_KEY = "AC8C8396F9F516FCE01A6805FF8C3D8E"
+
 # Taylor's Unit Test
 
 
