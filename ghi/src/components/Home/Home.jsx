db2e7e92ac02b45da95167473db29bc6b6fa3f80
Hamza <hamzasahmed11@gitlab.com>
2023-12-11 10:34:48 -0500

    update ci/cd

--- ghi/src/components/Home/Home.jsx
+++ ghi/src/components/Home/Home.jsx
@@ -12,13 +12,19 @@ function Home() {
   const [count, setCount] = useState(0);
 
   const getUser = async (e) => {
-    if (token !== null) {
-      const getToken = await fetch(
+    try {
+      if (token !== null) {
+        const getToken = await fetch(
         `${import.meta.env.VITE_REACT_APP_API_HOST}/token`,
         {
           credentials: "include",
         }
       );
+    } catch error {
+      setErrorMessage(
+        "Please login or signup"
+      );
+    }
       if (getToken.ok) {
         const data = await getToken.json();
         if (data) {
